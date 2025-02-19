9d865c572de50b271e309723f153c39b24519a69
LAPTOP-E6T9QUMT\Tibame <a1991818@gmail.com>
2025-02-19 20:31:29 +0800

    新增一些變數名稱，包含int, double, float, string

--- MyApp1/Form1.cs
+++ MyApp1/Form1.cs
@@ -10,6 +10,15 @@ namespace MyApp1
         private void Form1_Load(object sender, EventArgs e)
         {
             //Hello Git!!
+            int a;
+            string b;
+            double c;
+            float d;
+            a=c+d;
+            b=a.ToString;
+        }
+        public string fn1(int a){
+            
         }
     }
 }
